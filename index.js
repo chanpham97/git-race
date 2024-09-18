84a2491aa8b70bc352437ae46e2fa9f0f4f028f8
Chan Pham <iamchanpham@gmail.com>
2024-09-18 02:00:48 +0000

    Adding tasks in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Task 1
+// Print your name
+
+// Stage and commit this change with an appropriate message
+// Task 2
+// Print your favorite snack
+
+// Stage and commit this change with an appropriate message
+// Task 3
+// Print your favorite artist
+
+// Stage and commit this change with an appropriate message
+// Go to more.js
